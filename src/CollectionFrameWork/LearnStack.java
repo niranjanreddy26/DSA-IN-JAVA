c8c57dc6c18999a7d74525932c8833945f8d99bf
NIRANJAN REDDY KALUVAI <niranjanreddy2626@gmail.com>
2024-10-08 12:29:43 +0530

    stack

--- src/CollectionFrameWork/LearnStack.java
+++ src/CollectionFrameWork/LearnStack.java
@@ -5,12 +5,13 @@ import java.util.Stack;
 public class LearnStack {
     public static void main(String[] args) {
         Stack<String> heros = new Stack<>();
-        heros.push("zooma");
+        heros.push("zooma");// pushing elements into the stack
         heros.push("bhAAi");
         heros.push("cheddy");
         heros.push("pawala");
-        System.out.println(heros);
-        System.out.println(heros.pop());
+        System.out.println(heros);// display the stack elements
+        System.out.println(heros.peek()); // seeing the top most element in the stack
+        System.out.println(heros.pop());// removing the top element from the stack
         System.out.println(heros.peek());
     }
 }
